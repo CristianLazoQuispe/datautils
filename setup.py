37308751b363180a3dadd9da8e27b74870bfa5c7
CristianLazoQuispe <mecatronico.lazo@gmail.com>
2022-05-28 08:14:54 -0500

    review readme in setup

--- setup.py
+++ setup.py
@@ -11,6 +11,5 @@ setup(
     author='Cristian Lazo Quispe',
     author_email='mecatronico.lazo@gmail.com',
     license='MIT',
-    long_description=open('README.txt').read(),
     url='https://github.com/CristianLazoQuispe/datautils'
 )
